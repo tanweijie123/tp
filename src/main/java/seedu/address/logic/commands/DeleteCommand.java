1513dca6056e001606b1d1076616838cf6471201
kelvinvin <kelvinw0ng@hotmail.com>
2020-09-25 14:36:29 +0800

    Revert bad commit

--- src/main/java/seedu/address/logic/commands/DeleteCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteCommand.java
@@ -17,7 +17,7 @@ import seedu.address.model.client.Client;
  */
 public class DeleteCommand extends Command {
 
-    public static final String COMMAND_WORD = "cdel";
+    public static final String COMMAND_WORD = "delete";
 
     public static final String MESSAGE_USAGE = COMMAND_WORD
             + ": Deletes the Client identified by the index number used in the displayed Client list.\n"
