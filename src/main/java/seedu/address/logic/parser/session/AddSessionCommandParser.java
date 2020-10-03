d7dc7ed5a3a27a6d7f917a6bc54ec09de32a2a18
Bennett Clement <benclmnt.dev@gmail.com>
2020-10-03 19:13:20 +0800

    Add SessionCommand parser

---
+++ src/main/java/seedu/address/logic/parser/session/AddSessionCommandParser.java
@@ -0,0 +1,63 @@
+package seedu.address.logic.parser.session;
+
+import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.session.CliSyntax.PREFIX_DURATION;
+import static seedu.address.logic.parser.session.CliSyntax.PREFIX_EXERCISE_TYPE;
+import static seedu.address.logic.parser.session.CliSyntax.PREFIX_GYM;
+import static seedu.address.logic.parser.session.CliSyntax.PREFIX_START_TIME;
+
+import java.util.stream.Stream;
+
+import seedu.address.logic.commands.session.AddSessionCommand;
+import seedu.address.logic.parser.ArgumentMultimap;
+import seedu.address.logic.parser.ArgumentTokenizer;
+import seedu.address.logic.parser.Parser;
+import seedu.address.logic.parser.Prefix;
+import seedu.address.logic.parser.exceptions.ParseException;
+import seedu.address.model.session.ExerciseType;
+import seedu.address.model.session.Gym;
+import seedu.address.model.session.Interval;
+import seedu.address.model.session.Session;
+
+/**
+ * Parses input arguments and creates a new AddCommand object
+ */
+public class AddSessionCommandParser implements Parser<AddSessionCommand> {
+
+    /**
+     * Returns true if none of the prefixes contains empty {@code Optional} values in the given
+     * {@code ArgumentMultimap}.
+     */
+    private static boolean arePrefixesPresent(ArgumentMultimap argumentMultimap, Prefix... prefixes) {
+        return Stream.of(prefixes).allMatch(prefix -> argumentMultimap.getValue(prefix).isPresent());
+    }
+
+    /**
+     * Parses the given {@code String} of arguments in the context of the AddSessionCommand
+     * and returns an AddSessionCommand object for execution.
+     *
+     * @throws ParseException if the user input does not conform the expected format
+     */
+    public AddSessionCommand parse(String args) throws ParseException {
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer.tokenize(args, PREFIX_GYM, PREFIX_EXERCISE_TYPE, PREFIX_START_TIME, PREFIX_DURATION);
+
+        if (!arePrefixesPresent(argMultimap, PREFIX_GYM, PREFIX_EXERCISE_TYPE, PREFIX_START_TIME, PREFIX_DURATION)
+                || !argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, AddSessionCommand.MESSAGE_USAGE));
+        }
+
+        Gym gym = SessionParserUtil.parseGym(argMultimap.getValue(PREFIX_GYM).get());
+        Interval interval = SessionParserUtil.parseInterval(
+                argMultimap.getValue(PREFIX_START_TIME).get(),
+                argMultimap.getValue(PREFIX_DURATION).get()
+        );
+        ExerciseType exerciseType = SessionParserUtil.parseExerciseType(argMultimap.getValue(PREFIX_START_TIME).get());
+
+        Session session = new Session(gym, exerciseType, interval);
+
+        return new AddSessionCommand(session);
+    }
+
+}
+
