155c8338de1ae92cb52c49e672a5c56240e615e0
dhafin <dhafinrazaq@gmail.com>
2020-10-10 18:14:01 +0800

    Change session identifier to interval

--- src/main/java/seedu/address/model/util/SampleDataUtil.java
+++ src/main/java/seedu/address/model/util/SampleDataUtil.java
@@ -26,34 +26,34 @@ import seedu.address.model.tag.Tag;
 public class SampleDataUtil {
     public static Client[] getSampleClients() {
         return new Client[]{
-            new Client(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
-                    new Address("Blk 30 Geylang Street 29, #06-40"),
-                    getTagSet("injured-thigh")),
-            new Client(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
-                    new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
-                    getTagSet("allergy-nuts", "injured-thigh")),
-            new Client(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
-                    new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
-                    getTagSet("injured-back")),
-            new Client(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
-                    new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
-                    getTagSet("allergy-dairy")),
-            new Client(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
-                    new Address("Blk 47 Tampines Street 20, #17-35"),
-                    getTagSet("injured-thumb")),
-            new Client(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
-                    new Address("Blk 45 Aljunied Street 85, #11-31"),
-                    getTagSet("allergy-nuts"))
+                new Client(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
+                        new Address("Blk 30 Geylang Street 29, #06-40"),
+                        getTagSet("injured-thigh")),
+                new Client(new Name("Bernice Yu"), new Phone("99272758"), new Email("berniceyu@example.com"),
+                        new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
+                        getTagSet("allergy-nuts", "injured-thigh")),
+                new Client(new Name("Charlotte Oliveiro"), new Phone("93210283"), new Email("charlotte@example.com"),
+                        new Address("Blk 11 Ang Mo Kio Street 74, #11-04"),
+                        getTagSet("injured-back")),
+                new Client(new Name("David Li"), new Phone("91031282"), new Email("lidavid@example.com"),
+                        new Address("Blk 436 Serangoon Gardens Street 26, #16-43"),
+                        getTagSet("allergy-dairy")),
+                new Client(new Name("Irfan Ibrahim"), new Phone("92492021"), new Email("irfan@example.com"),
+                        new Address("Blk 47 Tampines Street 20, #17-35"),
+                        getTagSet("injured-thumb")),
+                new Client(new Name("Roy Balakrishnan"), new Phone("92624417"), new Email("royb@example.com"),
+                        new Address("Blk 45 Aljunied Street 85, #11-31"),
+                        getTagSet("allergy-nuts"))
         };
     }
 
     public static Session[] getSampleSession() {
         try {
             return new Session[]{
-                new Session(new Gym("Getwell gym"), new ExerciseType("Endurance"),
-                        new Interval(SessionParserUtil.parseStringToDateTime("29/09/2020 1300"), 120)),
-                new Session(new Gym("Machoman gym"), new ExerciseType("Bodybuilder"),
-                        new Interval(SessionParserUtil.parseStringToDateTime("29/09/2020 1600"), 150))
+                    new Session(new Gym("Getwell gym"), new ExerciseType("Endurance"),
+                            new Interval(SessionParserUtil.parseStringToDateTime("29/09/2020 1300"), 120)),
+                    new Session(new Gym("Machoman gym"), new ExerciseType("Bodybuilder"),
+                            new Interval(SessionParserUtil.parseStringToDateTime("29/09/2020 1600"), 150))
             };
         } catch (ParseException e) {
             //INFO: if you reach here, it means the date input above is wrong.
@@ -65,17 +65,17 @@ public class SampleDataUtil {
     public static Schedule[] getSampleSchedule() {
         try {
             return new Schedule[]{
-                new Schedule(new Client(new Name("Bernice Yu"), new Phone("99272758"),
-                        new Email("berniceyu@example.com"),
-                        new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
-                        getTagSet("allergy-nuts", "injured-thigh")),
-                    new Session(1, "Getwell gym", "Endurance",
-                        SessionParserUtil.parseStringToDateTime("29/09/2020 1300"), 120)),
-                new Schedule(new Client(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
-                        new Address("Blk 30 Geylang Street 29, #06-40"),
-                        getTagSet("injured-thigh")),
-                    new Session(2, "Machoman gym", "Bodybuilder",
-                        SessionParserUtil.parseStringToDateTime("29/09/2020 1600"), 150))
+                    new Schedule(new Client(new Name("Bernice Yu"), new Phone("99272758"),
+                            new Email("berniceyu@example.com"),
+                            new Address("Blk 30 Lorong 3 Serangoon Gardens, #07-18"),
+                            getTagSet("allergy-nuts", "injured-thigh")),
+                            new Session(new Gym("Getwell gym"), new ExerciseType("Endurance"),
+                                    new Interval(SessionParserUtil.parseStringToDateTime("29/09/2020 1300"), 120))),
+                    new Schedule(new Client(new Name("Alex Yeoh"), new Phone("87438807"), new Email("alexyeoh@example.com"),
+                            new Address("Blk 30 Geylang Street 29, #06-40"),
+                            getTagSet("injured-thigh")),
+                            new Session(new Gym("Machoman gym"), new ExerciseType("Bodybuilder"),
+                                    new Interval(SessionParserUtil.parseStringToDateTime("29/09/2020 1600"), 150)))
             };
         } catch (ParseException e) {
             //INFO: if you reach here, it means the date input above is wrong.
--- src/main/java/seedu/address/storage/JsonAdaptedSchedule.java
+++ src/main/java/seedu/address/storage/JsonAdaptedSchedule.java
@@ -5,25 +5,27 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.commons.util.StringUtil;
+import seedu.address.logic.parser.session.SessionParserUtil;
 import seedu.address.model.client.Email;
 import seedu.address.model.schedule.Schedule;
+import seedu.address.model.session.Interval;
 
 public class JsonAdaptedSchedule {
     public static final String MISSING_FIELD_MESSAGE_FORMAT = "Schedule's %s field is missing!";
     public static final String MISSING_CLIENT_EMAIL_MESSAGE_FORMAT = "Schedule's %s client email is missing!";
-    public static final String MISSING_SESSION_ID_MESSAGE_FORMAT = "Schedule's %s session ID is missing!";
+    public static final String MISSING_SESSION_INTERVAL_MESSAGE_FORMAT = "Schedule's %s session Interval is missing!";
 
     private final String clientEmail;
-    private final String sessionId;
+    private final String sessionInterval;
 
     /**
      * Constructs a {@code JsonAdaptedSchedule} with the given Schedule details.
      */
     @JsonCreator
     public JsonAdaptedSchedule(@JsonProperty("clientEmail") String clientEmail,
-                              @JsonProperty("sessionId") String sessionId) {
+                               @JsonProperty("sessionInterval") String sessionInterval) {
         this.clientEmail = clientEmail;
-        this.sessionId = sessionId;
+        this.sessionInterval = sessionInterval;
     }
 
     /**
@@ -31,7 +33,7 @@ public class JsonAdaptedSchedule {
      */
     public JsonAdaptedSchedule(Schedule source) {
         clientEmail = source.getClient().getEmail().toString();
-        sessionId = "" + source.getSession().getId();
+        sessionInterval = source.getSession().getInterval().toString();
     }
 
     /**
@@ -55,18 +57,19 @@ public class JsonAdaptedSchedule {
     }
 
     /**
-     * Converts a given {@code Schedule} into this class for Jackson use.
+     * Converts this Jackson-friendly adapted Schedule object to get its model's {@code Interval} object.
      *
      * @throws IllegalValueException if there were any data constraints violated in the adapted id.
      */
-    public int getSessionId() throws IllegalValueException {
-        if (sessionId == null || !StringUtil.isNonNegativeInteger(sessionId)) {
-            throw new IllegalValueException(String.format(MISSING_SESSION_ID_MESSAGE_FORMAT,
-                    Integer.class.getSimpleName()));
+    public Interval getSessionInterval() throws IllegalValueException {
+        if (sessionInterval == null) {
+            throw new IllegalValueException(String.format(MISSING_SESSION_INTERVAL_MESSAGE_FORMAT,
+                    Interval.class.getSimpleName()));
         }
 
-        final int modelSessionId = Integer.parseInt(sessionId);
+        String[] components = sessionInterval.split(" - ");
+        final Interval modelSessionInterval = SessionParserUtil.parseInterval(components[0], components[1]);
 
-        return modelSessionId;
+        return modelSessionInterval;
     }
 }
