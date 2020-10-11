f530a839f4ccae34c9d337221c75509c708035e9
kelvinvin <kelvinw0ng@hotmail.com>
2020-10-11 21:49:31 +0800

    Remove upcoming session in rightSideBar -> Duplicated in home

--- src/main/java/seedu/address/ui/RightSideBar.java
+++ src/main/java/seedu/address/ui/RightSideBar.java
@@ -4,7 +4,6 @@ import java.util.logging.Logger;
 
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.AnchorPane;
@@ -13,15 +12,12 @@ import seedu.address.model.session.Session;
 
 public class RightSideBar extends UiPart<AnchorPane> {
     private static final String FXML = "RightSideBar.fxml";
-    private static ObservableList<Session> sessionList;
     private final Logger logger = LogsCenter.getLogger(RightSideBar.class);
     private final MainWindow mainWindow;
 
 
     @FXML
     private ListView<Session> sessionListView;
-    @FXML
-    private Label lblNextClient;
 
     /**
      * Creates a {@code RightSideBar} with the given {@code ObservableList}.
@@ -29,21 +25,8 @@ public class RightSideBar extends UiPart<AnchorPane> {
     public RightSideBar(MainWindow mainWindow, ObservableList<Session> sessionList) {
         super(FXML);
         this.mainWindow = mainWindow;
-        this.sessionList = sessionList;
         sessionListView.setItems(sessionList);
         sessionListView.setCellFactory(listView -> new RightSideBar.SessionListViewCell());
-        updateUpcomingSession();
-    }
-
-    /**
-     * Initialises the nearest upcoming session shown.
-     */
-    public void updateUpcomingSession() {
-        if (this.sessionList.isEmpty()) {
-            lblNextClient.setText("-");
-        } else {
-            lblNextClient.setText(sessionList.get(0).getInterval().toString());
-        }
     }
 
     /**
