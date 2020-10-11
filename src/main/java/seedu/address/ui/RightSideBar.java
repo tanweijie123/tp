e51438d54ec06fece663cb75976105679f7daaa3
kelvinvin <kelvinw0ng@hotmail.com>
2020-10-11 18:15:13 +0800

    Updates upcoming session on load

--- src/main/java/seedu/address/ui/RightSideBar.java
+++ src/main/java/seedu/address/ui/RightSideBar.java
@@ -4,6 +4,7 @@ import java.util.logging.Logger;
 
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.AnchorPane;
@@ -12,11 +13,15 @@ import seedu.address.model.session.Session;
 
 public class RightSideBar extends UiPart<AnchorPane> {
     private static final String FXML = "RightSideBar.fxml";
+    private static ObservableList<Session> sessionList;
     private final Logger logger = LogsCenter.getLogger(RightSideBar.class);
     private final MainWindow mainWindow;
 
+
     @FXML
     private ListView<Session> sessionListView;
+    @FXML
+    private Label lblNextClient;
 
     /**
      * Creates a {@code RightSideBar} with the given {@code ObservableList}.
@@ -24,9 +29,23 @@ public class RightSideBar extends UiPart<AnchorPane> {
     public RightSideBar(MainWindow mainWindow, ObservableList<Session> sessionList) {
         super(FXML);
         this.mainWindow = mainWindow;
+        this.sessionList = sessionList;
         sessionListView.setItems(sessionList);
         sessionListView.setCellFactory(listView -> new RightSideBar.SessionListViewCell());
+        updateUpcomingSession();
     }
+
+    /**
+     * Initialises the nearest upcoming session shown.
+     */
+    public void updateUpcomingSession() {
+        if (this.sessionList.isEmpty()) {
+            lblNextClient.setText("-");
+        } else {
+            lblNextClient.setText(sessionList.get(0).getInterval().toString());
+        }
+    }
+
     /**
      * Custom {@code ListCell} that displays the graphics of a {@code Session} using a {@code SessionCard}.
      */
