cc87cbd028e6babfb487b376dfb9a9f15724a622
Tan Wei Jie <tanweijie123@gmail.com>
2020-10-11 14:22:47 +0800

    Remove Stage and Scene from ClientInfoPage (dead codes)

--- src/main/java/seedu/address/ui/ClientInfoPage.java
+++ src/main/java/seedu/address/ui/ClientInfoPage.java
@@ -3,16 +3,14 @@ package seedu.address.ui;
 import java.util.Comparator;
 
 import javafx.fxml.FXML;
-import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
-import seedu.address.commons.util.AppUtil;
 import seedu.address.model.client.Client;
 
-public class ClientInfoPage extends UiPart<AnchorPane> implements Ui {
+public class ClientInfoPage extends UiPart<AnchorPane> {
     private static final String FXML = "ClientInfoPage.fxml";
     private static ClientInfoPage clientInfoPage;
     private final Client client;
@@ -70,31 +68,5 @@ public class ClientInfoPage extends UiPart<AnchorPane> implements Ui {
         // state check
         ClientInfoPage card = (ClientInfoPage) other;
         return client.equals(card.client);
-    }
-
-    /**
-     * This method is not used if it is embbed in MAIN GUI.
-     * @param primaryStage
-     */
-    @Override
-    public void start(Stage primaryStage) {
-        Scene scene = new Scene(getRoot());
-        primaryStage.setScene(scene);
-        primaryStage.setTitle("View Client Profile");
-        primaryStage.getIcons().add(AppUtil.getImage("/images/profile_info.png"));
-        this.stage = primaryStage;
-    }
-
-    /**
-     * Shows the Profile to the user.
-     * If there is another client info page opened, it will be closed.
-     */
-    public void show() {
-        if (clientInfoPage != null) {
-            Stage stage = (Stage) clientInfoPage.lblName.getScene().getWindow();
-            stage.close();
-        }
-        this.stage.show();
-        clientInfoPage = this;
     }
 }
