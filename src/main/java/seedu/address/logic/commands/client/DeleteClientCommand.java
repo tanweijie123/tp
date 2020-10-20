bedd3ecd6282305fbc363abf8e9456bdd9f770d9
dhafin <dhafinrazaq@gmail.com>
2020-10-20 23:34:03 +0800

    Improve code quality

--- src/main/java/seedu/address/logic/commands/client/DeleteClientCommand.java
+++ src/main/java/seedu/address/logic/commands/client/DeleteClientCommand.java
@@ -69,14 +69,11 @@ public class DeleteClientCommand extends Command {
         // Do not delete Client unless user use the force flag
         if (model.hasAnyScheduleAssociatedWithClient(clientToDelete) && !isForced) {
             return new CommandResult(MESSAGE_FORCE_DELETE_CLIENT_USAGE);
+        } else {
+            model.deleteClientAssociatedSchedules(clientToDelete);
+            model.deleteClient(clientToDelete);
+            return new CommandResult(String.format(MESSAGE_DELETE_CLIENT_SUCCESS, clientToDelete));
         }
-
-        assert isForced || !model.hasAnyScheduleAssociatedWithClient(clientToDelete);
-
-        model.deleteClientAssociatedSchedules(clientToDelete);
-        model.deleteClient(clientToDelete);
-
-        return new CommandResult(String.format(MESSAGE_DELETE_CLIENT_SUCCESS, clientToDelete));
     }
 
     @Override
