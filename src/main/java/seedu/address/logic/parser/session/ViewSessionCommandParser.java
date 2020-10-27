f5970ba5ddc431358511c4e7ab1486b3d140431f
kelvinvin <kelvinw0ng@hotmail.com>
2020-10-27 14:43:45 +0800

    Revert "Refactor ViewSessionCommand"
    This reverts commit 30fedc132701d2f8dc785d6080a3e4d230b645cb.

--- src/main/java/seedu/address/logic/parser/session/ViewSessionCommandParser.java
+++ src/main/java/seedu/address/logic/parser/session/ViewSessionCommandParser.java
@@ -1,6 +1,8 @@
 package seedu.address.logic.parser.session;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.commands.session.ViewSessionCommand.PREDICATE_HASH_MAP;
+import static seedu.address.logic.commands.session.ViewSessionCommand.VALID_PATTERN;
 import static seedu.address.logic.parser.session.CliSyntax.PREFIX_PERIOD;
 
 import seedu.address.logic.commands.session.ViewSessionCommand;
@@ -15,6 +17,16 @@ import seedu.address.logic.parser.exceptions.ParseException;
 public class ViewSessionCommandParser implements Parser<ViewSessionCommand> {
 
     /**
+     * Checks if a period provided by the user is recognised
+     *
+     * @param period a String to check whether if it is a valid period.
+     * @return true if period is valid, false otherwise.
+     */
+    public static boolean isValidPeriod(String period) {
+        return PREDICATE_HASH_MAP.containsKey(period) || VALID_PATTERN.matcher(period).matches();
+    }
+
+    /**
      * Parses the given {@code String} of arguments in the context of the ViewSessionCommand
      * and returns an ViewSessionCommand object for execution.
      *
@@ -24,9 +36,11 @@ public class ViewSessionCommandParser implements Parser<ViewSessionCommand> {
         ArgumentMultimap argMultimap =
                 ArgumentTokenizer.tokenize(args, PREFIX_PERIOD);
 
-        if (argMultimap.getValue(PREFIX_PERIOD).isEmpty() || !argMultimap.getPreamble().isEmpty()) {
+        if (argMultimap.getValue(PREFIX_PERIOD).isEmpty() || !argMultimap.getPreamble().isEmpty()
+                || !isValidPeriod(argMultimap.getValue(PREFIX_PERIOD).get())) {
             throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT, ViewSessionCommand.MESSAGE_USAGE));
         }
+
         return new ViewSessionCommand(argMultimap.getValue(PREFIX_PERIOD).get());
     }
 }
