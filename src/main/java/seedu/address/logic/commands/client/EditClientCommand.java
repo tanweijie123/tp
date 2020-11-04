4f6d5852372c4c318d016ff17b4145abf9cabe06
dhafin <dhafinrazaq@gmail.com>
2020-11-04 15:57:18 +0800

    Update add schedule javadocs

--- src/main/java/seedu/address/logic/commands/client/EditClientCommand.java
+++ src/main/java/seedu/address/logic/commands/client/EditClientCommand.java
@@ -83,6 +83,7 @@ public class EditClientCommand extends Command {
         Client clientToEdit = lastShownList.get(index.getZeroBased());
         Client editedClient = createEditedClient(clientToEdit, editClientDescriptor);
 
+        // Edit the Client if currently no identical Client exist
         if (!clientToEdit.isIdentical(editedClient) && model.hasClient(editedClient)) {
             throw new CommandException(MESSAGE_DUPLICATE_CLIENT);
         }
--- src/main/java/seedu/address/logic/parser/client/EditClientCommandParser.java
+++ src/main/java/seedu/address/logic/parser/client/EditClientCommandParser.java
@@ -31,7 +31,7 @@ public class EditClientCommandParser implements Parser<EditClientCommand> {
     /**
      * Parses the given {@code String} of arguments in the context of the EditClientCommand
      * and returns an EditClientCommand object for execution.
-     * @throws ParseException if the user input does not conform the expected format
+     * @throws ParseException if the user input does not conform the expected format.
      */
     public EditClientCommand parse(String args) throws ParseException {
         requireNonNull(args);
