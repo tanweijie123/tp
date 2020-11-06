60d224f9fe67ec0a26315d9cd5614efd14c8779a
Tan Wei Jie <tanweijie123@gmail.com>
2020-11-06 19:09:08 +0800

    Change to display notif with exception

--- src/main/java/seedu/address/MainApp.java
+++ src/main/java/seedu/address/MainApp.java
@@ -6,12 +6,15 @@ import java.util.Optional;
 import java.util.logging.Logger;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonBar;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.Region;
 import javafx.stage.Stage;
+import javafx.stage.Window;
 import seedu.address.commons.core.Config;
 import seedu.address.commons.core.LogsCenter;
 import seedu.address.commons.core.Version;
@@ -89,9 +92,29 @@ public class MainApp extends Application {
             initialData = addressBookOptional.orElseGet(SampleDataUtil::getSampleAddressBook);
         } catch (DataConversionException e) {
             logger.warning("Data file not in the correct format. Will be starting with an empty AddressBook");
+
+            //Create a Warning Notification
+            String title = "Data file corrupted";
+            String headerText = "Data file not in correct format. "
+                    + "This could be caused by corrupted data from forced shut, or manually data modification. ";
+            String contentText = "To prevent overwriting your data, you are recommended to exit the program. "
+                    + "Otherwise, FitEgo will be starting with a blank state";
+
+            Platform.runLater(() -> createAlertWithErrorMessage(title, headerText, contentText, e));
+
             initialData = new AddressBook();
         } catch (IOException e) {
             logger.warning("Problem while reading from the file. Will be starting with an empty AddressBook");
+
+            //Create a Warning Notification
+            String title = "Unable to read from file";
+            String headerText = "Another software could be using the same file, "
+                    + "or you do not have the read permission to this file.";
+            String contentText = "To prevent overwriting your data, you are recommended to exit the program. "
+                    + "Otherwise, FitEgo will be starting with a blank state";
+
+            Platform.runLater(() -> createAlertWithErrorMessage(title, headerText, contentText, e));
+
             initialData = new AddressBook();
         }
 
@@ -174,49 +197,65 @@ public class MainApp extends Application {
     public void start(Stage primaryStage) {
         logger.info("Starting AddressBook " + MainApp.VERSION);
         ui.start(primaryStage);
-        checkEmptyAddressBook(primaryStage);
+
+        //get the first window and request focus
+        //if there is an error dialog, it will be focused, otherwise, FitEgo app will be focused.
+        Stage.getWindows().stream().filter(Window::isShowing).limit(1).forEach(x -> x.requestFocus());
     }
 
     /**
-     * Checks if FitEgo is starting with an empty AddressBook. If true, there might be error in reading saved file.
-     * This method will prompt for user's action to continue or exit program to review their data.
-     *
-     * @param primaryStage The main FitEgo application window
+     * Creates an alert window with the given title, header text, content text, and exception
      */
-    private void checkEmptyAddressBook(Stage primaryStage) {
-        if (model.getAddressBook().equals(new AddressBook())) {
-            //started with a blank state.
+    private void createAlertWithErrorMessage(String title, String headerText, String contentText, Exception e) {
+        ButtonType exceptionMessage = new ButtonType("Show Technical Error Message", ButtonBar.ButtonData.OTHER);
+        ButtonType cont = new ButtonType("Continue with Empty Data", ButtonBar.ButtonData.OK_DONE);
+        ButtonType exit = new ButtonType("Exit and Review my data file", ButtonBar.ButtonData.CANCEL_CLOSE);
 
-            ButtonType cont = new ButtonType("Continue with Empty Data", ButtonBar.ButtonData.OK_DONE);
-            ButtonType exit = new ButtonType("Exit and Review my data file", ButtonBar.ButtonData.CANCEL_CLOSE);
+        Alert alert = new Alert(Alert.AlertType.WARNING, "", exceptionMessage, cont, exit);
+        alert.setTitle(title);
+        alert.setHeaderText(headerText);
+        alert.setContentText(contentText);
 
-            Alert alert = new Alert(Alert.AlertType.WARNING, "", cont, exit);
-            alert.setTitle("Empty Address Book");
-            alert.setHeaderText("FitEgo is unable to load your saved file.");
-            alert.setContentText(
-                    "It could be possible that the software was force-shut, "
-                    + "the data is manipulated or you had cleared the data on your previous use. "
-                    + "To prevent overwriting your data, you are recommended to exit now; "
-                    + "otherwise, FitEgo will be starting with a blank state."
-            );
+        //Deactivate focus on OK button
+        Button contButton = (Button) alert.getDialogPane().lookupButton(cont);
+        contButton.setDefaultButton(false);
 
-            //Deactivate focus on OK button
-            Button contButton = (Button) alert.getDialogPane().lookupButton(cont);
-            contButton.setDefaultButton(false);
+        //Activate focus on Exit button
+        Button exitButton = (Button) alert.getDialogPane().lookupButton(exit);
+        exitButton.setDefaultButton(true);
 
-            //Activate focus on Exit button
-            Button exitButton = (Button) alert.getDialogPane().lookupButton(exit);
-            exitButton.setDefaultButton(true);
+        //Prevent alert from truncating
+        alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
+        Stage alertStage = (Stage) alert.getDialogPane().getScene().getWindow();
 
-            //Prevent alert from truncating
-            alert.getDialogPane().setMinHeight(Region.USE_PREF_SIZE);
+        //set focus to always be on top
+        alertStage.setAlwaysOnTop(true);
 
-            //Perform user action
-            Optional<ButtonType> result = alert.showAndWait();
-            if (result.orElse(exit) == exit) { //default to exit
-                primaryStage.close();
-            }
+        //Perform user action
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.orElse(exit) == exit) { //default to exit
+            Platform.exit();
+        } else if (result.get() == exceptionMessage) {
+            displayErrorStackTrace(e);
+            createAlertWithErrorMessage(title, headerText, contentText, e); //re-open dialog. potential stack overflow
         }
+    }
+
+    /**
+     * Displays an Error Dialog with the given exception.
+     */
+    private void displayErrorStackTrace(Exception e) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Exception Dialog");
+        alert.setHeaderText(e.getClass().getSimpleName());
+        alert.setContentText("This error might be caused by corrupted data file.");
+
+        TextArea textArea = new TextArea(e.getMessage());
+        textArea.setEditable(false);
+        textArea.setWrapText(true);
+
+        alert.getDialogPane().setExpandableContent(textArea);
+        alert.showAndWait();
     }
 
     @Override
